1dc923144f1c97715b32e4b4656e0197ee1adbe1
HamzaQahoush <hamza.qah@gmail.com>
2022-03-04 20:26:26 +0300

    add validation and some features

--- custom/task1/models/sale.py
+++ custom/task1/models/sale.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, tools
+from odoo.exceptions import ValidationError
 
 
 class SalesOrderInherit(models.Model):
@@ -9,22 +10,65 @@ class SalesOrderInherit(models.Model):
     #    order_line > available Quantity - > RFQ\
     #     quant_obj = self.env['stock.quant']
     #     qty_available = quant_obj._get_available_quantity(product_id)
+
     def check_available_products(self):
-        dic = {}
-        missing_qty = {}
+        """
+        - this method is to check all order-lines quantities  in sale-order and compare
+        it to available quantities.
+        return :: dict , contains the {product_id : missed Quantities}
+        for example : [{'product_id': 12, 'order_line_id': 12, 'missing': 1.0 , 'partner_id': 7}]
+        """
+        order_lines = []
+        global missing_qty
+        missing_qty = []
+        for rec in self:
+            print(rec['partner_id'])
         if self.order_line:
             for key in self.order_line:
-                dic[key['product_id']['id']] = key['product_qty']
-
-        for product_id in [*dic]:
-            product = self.env['product.product'].browse(product_id)
+                order_lines.append({'order_line_id': key['id'], 'product_id': key['product_id']['id'],
+                                    'partner_id': key['order_partner_id'].id,
+                                    'product_qty': key['product_qty'], 'price_unit': key['price_unit']})
+        for order in order_lines:
+            id = order.get('product_id')
+            product = self.env['product.product'].browse(id)
             available_qty = product.qty_available
-            if available_qty < dic[product_id]:
-                missing_qty[product_id] = dic[product_id] - available_qty
+            if available_qty < order['product_qty']:
+                missing_qty.append(
+                    {'product_id': order.get('product_id'), 'missing': order.get('product_qty') - available_qty,
+                     'order_line_id': order.get('order_line_id'), 'partner_id': order.get('partner_id'),
+                     'price_unit': order.get('price_unit')
+                     })
         print(missing_qty)
+
         return missing_qty
 
-    def create_po_button(self):
+    @staticmethod
+    def get_required_field(missing_qty, field):
+        field, = [x[str(field)] for x in missing_qty]
+        return field
+
+    def po_button(self):
+
+        missing_qty = self.check_available_products()
+        if not missing_qty:
+            raise ValidationError(
+                ' Your sale order will complete  No need to create PO.')
+
+        # print('missing_qty--------', missing_qty)
+        # order_line, = [x['order_line_id'] for x in missing_qty]
+
+        vals = {
+            'partner_id': self.get_required_field(missing_qty, 'partner_id'),
+            'order_line': [
+                (0, 0, {
+
+                    'product_id': self.get_required_field(missing_qty, 'product_id'),
+                    'product_qty': self.get_required_field(missing_qty, 'missing'),
+                    'price_unit': self.get_required_field(missing_qty, 'price_unit'),
+                })]
+
+        }
+        purchase_rec = self.env['purchase.order'].create(vals)
 
         return {
             'name': 'RFQ',
@@ -33,28 +77,22 @@ class SalesOrderInherit(models.Model):
             'view_id': self.env.ref('purchase.purchase_order_form').id,
             'res_model': 'purchase.order',
             'target': 'current',
-            'domain': [()]
+            'res_id': purchase_rec.id,
+
         }
 
         # sale_order_lines = self.env['sale.order.line'].sudo().browse(sale_line_ids | empty_line_ids)
 
-    # 'qty_available_today': 0.0
-    # 'qty_to_deliver': 1.0
-    # 'product_qty': 1.0
-    # 'product_uom_qty': 1.0
 
 
 class PurchaseOrderInherit(models.Model):
     _inherit = "purchase.order"
 
-    @api.model
+    @api.model_create_multi
     def create(self, vals):
         """
         triggerd on save
         """
-        # missed_qut_dic = SalesOrderInherit.check_available_products(self)
-        # print('missed_qut_dic------------------>', missed_qut_dic)
-
         # vals = {
         #     'product_id': 1,
         #     'partner_id': 1,
@@ -63,23 +101,27 @@ class PurchaseOrderInherit(models.Model):
         #
         # }
         res = super(PurchaseOrderInherit, self).create(vals)
-        print('vals create-------------------', vals.get('order_line')[0][2])
-        print('vals res create ', res)
+        # print('vals create-------------------', vals.get('order_line')[0][2])
+        # print('vals res create ', res)
         return res
 
-    @api.model
-    def default_get(self, fields):
-        """
-         - override the default get function and set default values for fields.
-         - res : will be a dic {key:value} for each field.
-         - triggered when clicking on create.
-         - fields is list []  contain all fields , and res will be dic {key:value}
-
-        """
-        res = super(PurchaseOrderInherit, self).default_get(fields)
-        print('res default_get-------------', res)
-        print('fields default_get-------------------', fields)
-        return res
+    # @api.model
+    # def default_get(self, fields):
+    #
+    #     print('missed_qut_dic self.helper() this is form PurchaseOrderInherit default_get------------------>')
+    #
+    #     """
+    #      - override the default get function and set default values for fields.
+    #      - res : will be a dic {key:value} for each field.
+    #      - triggered when clicking on create.
+    #      - fields is list []  contain all fields , and res will be dic {key:value}
+    #
+    #     """
+    #     res = super(PurchaseOrderInherit, self).default_get(fields)
+    #
+    #     print('res default_get-------------', res)
+    #     print('fields default_get-------------------', fields)
+    #     return res
 
 
 class StockQuant(models.Model):
